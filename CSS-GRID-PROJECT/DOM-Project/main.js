b8b795026917becfbef51667290238111f1fefec
Alex Basystyi <alexbasisty@gmail.com>
2020-05-03 13:25:41 +0200

    showInfo

--- CSS-GRID-PROJECT/DOM-Project/main.js
+++ CSS-GRID-PROJECT/DOM-Project/main.js
@@ -28,18 +28,33 @@ const searchForm = document.querySelector(".form--search");
 const searchElements = (e, element) => {
     e.preventDefault();
     const infoElement = document.querySelector(".result");
+
+    infoElement.textContent = "";
     const elements = document.querySelectorAll(element);
     if (elements.length > 0) {
         infoElement.innerHTML = `<p class="result__info">W tym dokumencie znalazłem <strong>${elements.length}</strong> elementów <strong>${element}</strong></p>`;
-        showInfo();
+        showInfo(elements, infoElement);
     } else {
         infoElement.innerHTML = `<p class="result__info">W tym dokumencie nie znalazłem elementów <strong>${element}</strong></p>`;
         return;
     }
 };
 
-const showInfo = () => {
-    console.log("hello");
+const showInfo = (elements, infoElement) => {
+    elements.forEach((el) => {
+        const item = document.createElement("div");
+        item.className = "element-info";
+        item.innerHTML = `
+        <div>${el.nodeName}</div>
+        <div>klasa/klasy: ${el.className}</div>
+        <div>Wysokość elementu(offsetHeight): ${el.offsetHeight}</div>
+        <div>Szerokość elementu(offsetWidth): ${el.offsetWidth}</div>
+        <div>Odległość od lewej krawędzi (offsetLeft): ${el.offsetLeft}</div>
+        <div>Odległość od górnej krawędzi (offsetTop): ${el.offsetTop}</div>
+        <div>Liczba elementów dzieci(childElementCount): ${el.childElementCount}</div>
+        `;
+        infoElement.appendChild(item);
+    });
 };
 
 searchForm.addEventListener("submit", (e) =>
