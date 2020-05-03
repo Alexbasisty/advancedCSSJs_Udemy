86c335890210076a6b8c680b74ee4c4fe13228fa
Alex Basystyi <alexbasisty@gmail.com>
2020-05-03 13:05:37 +0200

    searchForm listener

--- CSS-GRID-PROJECT/DOM-Project/main.js
+++ CSS-GRID-PROJECT/DOM-Project/main.js
@@ -22,3 +22,26 @@ addForm.addEventListener("submit", (e) =>
         addForm.elements.value.value
     )
 );
+
+const searchForm = document.querySelector(".form--search");
+
+const searchElements = (e, element) => {
+    e.preventDefault();
+    const infoElement = document.querySelector(".result");
+    const elements = document.querySelectorAll(element);
+    if (elements.length > 0) {
+        infoElement.innerHTML = `<p class="result__info">W tym dokumencie znalazłem <strong>${elements.length}</strong> elementów <strong>${element}</strong></p>`;
+        showInfo();
+    } else {
+        infoElement.innerHTML = `<p class="result__info">W tym dokumencie nie znalazłem elementów <strong>${element}</strong></p>`;
+        return;
+    }
+};
+
+const showInfo = () => {
+    console.log("hello");
+};
+
+searchForm.addEventListener("submit", (e) =>
+    searchElements(event, searchForm.elements["searching-element"].value)
+);
