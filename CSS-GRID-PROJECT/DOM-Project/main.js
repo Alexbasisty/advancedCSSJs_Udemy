3405a959731e7386e56963994b55bcf82f47e977
Alex Basystyi <alexbasisty@gmail.com>
2020-05-03 12:08:51 +0200

    addEventListener

--- CSS-GRID-PROJECT/DOM-Project/main.js
+++ CSS-GRID-PROJECT/DOM-Project/main.js
@@ -1,7 +1,17 @@
 const addForm = document.querySelector(".form--add");
 
-const addElement = (event) => {
+const addElement = (
+    event,
+    node = "div",
+    txt = "text",
+    attr = "style",
+    value = 'font-size = "20px"'
+) => {
     event.preventDefault();
+    const element = document.createElement(node);
+    const text = document.createTextNode(txt);
+    element.appendChild(text);
+    document.body.appendChild(element);
 };
 
 addForm.addEventListener("submit", addElement);
