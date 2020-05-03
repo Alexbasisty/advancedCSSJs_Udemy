663e7a9d25faceaf77f9957ed814b43e3f02aa77
Alex Basystyi <alexbasisty@gmail.com>
2020-05-03 12:32:38 +0200

    first event

--- CSS-GRID-PROJECT/DOM-Project/main.js
+++ CSS-GRID-PROJECT/DOM-Project/main.js
@@ -1,17 +1,20 @@
 const addForm = document.querySelector(".form--add");
 
-const addElement = (
-    event,
-    node = "div",
-    txt = "text",
-    attr = "style",
-    value = 'font-size = "20px"'
-) => {
+const addElement = (event, node, txt, attr, value) => {
     event.preventDefault();
     const element = document.createElement(node);
     const text = document.createTextNode(txt);
     element.appendChild(text);
+    element.setAttribute(attr, value);
     document.body.appendChild(element);
 };
 
-addForm.addEventListener("submit", addElement);
+addForm.addEventListener("submit", (e) =>
+    addElement(
+        e,
+        addForm.elements.node.value,
+        addForm.elements.text.value,
+        addForm.elements.attr.value,
+        addForm.elements.value.value
+    )
+);
