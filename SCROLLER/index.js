e910399aea1401886329e20778fbeab20a74e361
Alex Basystyi <alexbasisty@gmail.com>
2020-05-04 13:05:36 +0200

    set section index on scroll

--- SCROLLER/index.js
+++ SCROLLER/index.js
@@ -3,8 +3,21 @@ document.addEventListener("DOMContentLoaded", () => {
 
     const rootElement = document.getElementById("root");
     const sections = document.querySelectorAll("section");
+    let currSectionIndex = 0;
 
     document.addEventListener("mousewheel", (event) => {
-        console.log(event.wheelDelta);
+        const direction = event.wheelDelta < 0 ? 1 : -1;
+
+        if (direction === 1) {
+            const isLastSection = currSectionIndex === sections.length - 1;
+            if (isLastSection) return;
+        } else if (direction === -1) {
+            const isFirstSection = currSectionIndex === 0;
+            if (isFirstSection) return;
+        }
+
+        currSectionIndex = currSectionIndex + direction;
+
+        console.log(currSectionIndex);
     });
 });
