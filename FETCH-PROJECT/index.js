72eb956992597042b81a64ae2ab95aa467b9f52d
Alex Basystyi <alexbasisty@gmail.com>
2020-05-04 11:53:19 +0200

    fetch needed data from API

--- FETCH-PROJECT/index.js
+++ FETCH-PROJECT/index.js
@@ -1,9 +1,8 @@
 const getUsers = (e) => {
     e.preventDefault();
 
-    const usersNumber = document.querySelector('[(name = "users-number")]')
-        .value;
-    const usersGender = document.querySelector('[(name = "gender")]').value;
+    const usersNumber = document.querySelector('[name = "users-number"]').value;
+    const usersGender = document.querySelector('[name = "gender"]').value;
 
     const url = `https://randomuser.me/api/?results=${usersNumber}&gender=${
         usersGender === "both" ? "male,female" : usersGender
@@ -17,8 +16,23 @@ const getUsers = (e) => {
                 return response.json();
             }
         })
-        .then((json) => console.log(json))
+        .then((json) => showUsers(json.results))
         .catch((err) => console.log(`Error: ${err}`));
+};
+
+const showUsers = (users) => {
+    const resultArea = document.querySelector(".users-list");
+    users.forEach((user) => {
+        const item = document.createElement("div");
+        item.className = "user";
+        item.innerHTML = `
+        <div class="user__name">${user.name.title.toUpperCase()} ${
+            user.name.first
+        } ${user.name.last.toUpperCase()}</div>
+        <img src=${user.picture.medium} alt="user-photo" class="user__image">
+        `;
+        resultArea.appendChild(item);
+    });
 };
 
 document.querySelector(".generator").addEventListener("submit", getUsers);
