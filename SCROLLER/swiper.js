09dca40b634e6aafa93b79f41fb10c8e7d8970be
Alex Basystyi <alexbasisty@gmail.com>
2020-05-06 14:13:29 +0200

    events for mobiles

---
+++ SCROLLER/swiper.js
@@ -0,0 +1,55 @@
+class Swiper {
+    constructor() {
+        this.initialY = null;
+        this.initialX = null;
+
+        document.addEventListener("touchstart", (event) =>
+            this.startTouch(event)
+        );
+        document.addEventListener("touchmove", (event) =>
+            this.moveTouch(event)
+        );
+        this.events = {
+            swipeUp: new Event("swipeUp"),
+            swipeDown: new Event("swipeDown"),
+            swipeLeft: new Event("swipeLeft"),
+            swipeRight: new Event("swipeRight"),
+        };
+    }
+
+    startTouch(event) {
+        event.preventDefault();
+
+        this.initialX = event.tooches[0].clientX;
+        this.initialY = event.tooches[0].clientY;
+    }
+
+    moveTouch(event) {
+        // if (!this.initialX || !this.initialY) {
+        //     return;
+        // }
+        const currX = event.tooches[0].clientX;
+        const currY = event.tooches[0].clientY;
+
+        const diffX = this.initialX - currX;
+        const diffY = this.initialY - currY;
+
+        if (Math.abs(diffX) > Math.abs(diffY)) {
+            if (diffX > 0) {
+                document.dispatchEvent(this.events.swipeLeft);
+            } else {
+                document.dispatchEvent(this.events.swipeRight);
+            }
+        } else {
+            if (diffY > 0) {
+                document.dispatchEvent(this.events.swipeUp);
+            } else {
+                document.dispatchEvent(this.events.swipeDown);
+            }
+        }
+        this.initialX = null;
+        this.initialY = null;
+    }
+}
+
+new Swiper();
