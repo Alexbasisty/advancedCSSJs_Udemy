e3c308356a4b38f2b6490480426faf67f9ad9c64
Alex Basystyi <alexbasisty@gmail.com>
2020-05-07 13:06:55 +0200

    Animated component

--- animated-background/src/App.js
+++ animated-background/src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Animated from "./Animated";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <Animated
+                    className="cloud"
+                    src="/cloud.png"
+                    ratioX="0.36"
+                    ratioY="0.34"
+                />
+            </header>
+        </div>
+    );
 }
 
 export default App;
