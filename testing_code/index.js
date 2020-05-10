1042d466048e9b6086ec8c082dcf28046ff0e4c9
Alex Basystyi <alexbasisty@gmail.com>
2020-05-10 11:16:29 +0200

    algoritm, that moves all zeros to the end of array

--- testing_code/index.js
+++ testing_code/index.js
@@ -1,1 +1,12 @@
-document.addEventListener("DOMContentLoaded", () => {});
+document.addEventListener("DOMContentLoaded", () => {
+    // Write an algorithm that takes an array and moves all of the zeros to the end, preserving the order of the other elements.
+
+    var moveZeros = function (arr) {
+        // TODO: Program me
+        const noZero = arr.filter((el) => el !== 0);
+        const zero = arr.filter((el) => el === 0);
+
+        return [...noZero, ...zero];
+    };
+    console.log(moveZeros([false, 1, 0, 1, 2, 0, 1, 3, "a"]));
+});
