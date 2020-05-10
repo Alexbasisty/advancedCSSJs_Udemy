3db22b839aae0b9e9fcda9f7eb284f2a0873f238
Alex Basystyi <alexbasisty@gmail.com>
2020-05-10 13:55:57 +0200

    math modules

--- testing_nodeJS/components/multiply.js
+++ testing_nodeJS/components/multiply.js
@@ -2,5 +2,10 @@ const multiply = (...numbers) => {
     return numbers.reduce((sum, curr) => sum * curr);
 };
 
-module.exports.multiply = multiply;
-module.exports.description = "mnożenie to działanie matematyczne";
+// module.exports.multiply = multiply;
+// module.exports.description = "mnożenie to działanie matematyczne";
+
+module.exports = {
+    multiply,
+    description: "mnożenie to ...",
+};
