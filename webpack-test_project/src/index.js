8345f7fab6fbbb4b9967340af63389faacb3492b
Alex Basystyi <alexbasisty@gmail.com>
2020-05-16 18:58:18 +0200

    css loaders with examples

--- webpack-test_project/src/index.js
+++ webpack-test_project/src/index.js
@@ -1,5 +1,7 @@
 import { message, messageDOM } from "./tools/message.js";
+import "./components/footer";
 import info from "./data/title.txt";
+import "./css/style.css";
 
 message("działam po bandlingu!");
 messageDOM(info);
