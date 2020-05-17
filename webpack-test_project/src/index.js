a79e484b4df8a71fe158db8715e0fe1a14ad74ce
Alex Basystyi <alexbasisty@gmail.com>
2020-05-17 15:13:45 +0200

    class creator

--- webpack-test_project/src/index.js
+++ webpack-test_project/src/index.js
@@ -3,7 +3,13 @@ import "./components/footer";
 import info from "./data/title.txt";
 import "./sass/index.scss";
 import addImage from "./tools/image";
+import Creator from "./tools/creator";
 
 message("działam po bandlingu!");
 messageDOM(info);
 addImage("h1");
+const element1 = new Creator();
+element1.addBgc("red");
+const element2 = new Creator();
+element2.addBgc("blue");
+const element3 = new Creator();
