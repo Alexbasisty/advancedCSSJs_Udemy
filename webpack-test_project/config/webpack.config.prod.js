5c8b8fa85bff02661978efdd6bcc32ab09db872d
Alex Basystyi <alexbasisty@gmail.com>
2020-05-17 11:48:09 +0200

    copy webpack plugin

--- webpack-test_project/config/webpack.config.prod.js
+++ webpack-test_project/config/webpack.config.prod.js
@@ -2,6 +2,7 @@ const path = require("path");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const HTMLWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
     mode: "production",
@@ -37,5 +38,11 @@ module.exports = {
         new MiniCssExtractPlugin({
             filename: "css/[name]-[contenthash:6].css",
         }),
+        new CopyPlugin([
+            {
+                from: "public/images",
+                to: "images",
+            },
+        ]),
     ],
 };
