36484423a67a5c54864a4c5c3124a04395529e36
Alex Basystyi <alexbasisty@gmail.com>
2020-05-19 13:27:27 +0200

    functional constructor

--- projects/gallery-API/js/app.js
+++ projects/gallery-API/js/app.js
@@ -1,21 +1,61 @@
 import "../sass/style.scss";
 
-const Doggo = () => {
+function Doggo() {
   this.url = "https://dog.ceo/api/";
-};
+  this.imgEl = document.querySelector(".featured-dog img");
+  this.init();
+}
 
-Doggo.prototype.listBreeds = () => {
+Doggo.prototype.listBreeds = function () {
   return fetch(`${this.url}breeds/list/all`)
     .then((resp) => resp.json())
     .then((data) => data.message);
 };
-Doggo.prototype.getRandomImage = () => {
+
+Doggo.prototype.getRandomImage = function () {
   return fetch(`${this.url}breeds/image/random`)
     .then((resp) => resp.json())
     .then((data) => data.message);
 };
-Doggo.prototype.getRandomImageByBreed = (breed) => {
+Doggo.prototype.getRandomImageByBreed = function (breed) {
   return fetch(`${this.url}breed/${breed}/images/random`)
     .then((resp) => resp.json())
     .then((data) => data.message);
 };
+
+Doggo.prototype.init = function () {
+  this.getRandomImage().then((src) => this.imgEl.setAttribute("src", src));
+  this.listBreeds().then((breeds) => console.log(breeds));
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  new Doggo();
+});
+// class Doggo {
+//   constructor() {
+//     this.url = "https://dog.ceo/api/";
+//     this.imgEl = document.querySelector(".featured-dog img");
+//     this.init();
+//   }
+//   listBreeds() {
+//     return fetch(`${this.url}breeds/list/all`)
+//       .then((resp) => resp.json())
+//       .then((data) => data.message);
+//   }
+
+//   getRandomImage() {
+//     return fetch(`${this.url}breeds/image/random`)
+//       .then((resp) => resp.json())
+//       .then((data) => data.message);
+//   }
+//   getRandomImageByBreed(breed) {
+//     return fetch(`${this.url}breed/${breed}/images/random`)
+//       .then((resp) => resp.json())
+//       .then((data) => data.message);
+//   }
+
+//   init() {
+//     this.getRandomImage().then((src) => this.imgEl.setAttribute("src", src));
+//     this.listBreeds().then((breeds) => console.log(breeds));
+//   }
+// }
