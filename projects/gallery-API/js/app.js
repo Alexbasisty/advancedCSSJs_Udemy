d9f45424ffb99392187ccc55ce372c01adb48c0a
Alex Basystyi <alexbasisty@gmail.com>
2020-05-19 11:46:03 +0200

    first fetch

--- projects/gallery-API/js/app.js
+++ projects/gallery-API/js/app.js
@@ -1,1 +1,13 @@
 import "../sass/style.scss";
+
+function listBreeds() {
+  return fetch("https://dog.ceo/api/breeds/list/all")
+    .then((resp) => resp.json())
+    .then((data) => {
+      return data.message;
+    });
+}
+
+function getRandomImage() {
+  return fetch("https://dog.ceo/api/breeds/image/random");
+}
