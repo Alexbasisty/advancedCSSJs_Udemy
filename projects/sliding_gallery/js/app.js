bb818035cf1bc051c3ee44bc927ec84ef5718c0c
Alex Basystyi <alexbasisty@gmail.com>
2020-05-21 13:37:41 +0200

    slider functions

--- projects/sliding_gallery/js/app.js
+++ projects/sliding_gallery/js/app.js
@@ -4,10 +4,57 @@ document.addEventListener("DOMContentLoaded", () => {
   const imagesContainerEl = document.querySelector(".slider__images-container");
   const img1El = document.querySelector(".slider__image-container--first img");
   const img2El = document.querySelector(".slider__image-container--second img");
+  let drugging = false;
+  const imagesContainerLeftOffset = imagesContainerEl.offsetLeft;
+  const img1ContainerEl = document.querySelector(
+    ".slider__image-container--first"
+  );
+  const img2ContainerEl = document.querySelector(
+    ".slider__image-container--second"
+  );
+  const handleEl = document.querySelector(".slider__handle");
+  const dividerEL = document.querySelector(".slider__divider");
+  let imagesContainerElWidth;
 
-  window.addEventListener("resize", () => {
-    const imagesContainerElWidth = imagesContainerEl.offsetWidth;
+  function getOffset(clientX) {
+    const offset = clientX - imagesContainerLeftOffset;
+    if (offset < 0) {
+      return 0;
+    } else if (offset > imagesContainerElWidth) {
+      return imagesContainerElWidth;
+    } else {
+      return offset;
+    }
+  }
+
+  function move(clientX) {
+    const offset = getOffset(clientX);
+    const percent = (offset / imagesContainerElWidth) * 100;
+    dividerEL.style.left = `${percent}%`;
+    img2ContainerEl.style.width = `${percent}%`;
+  }
+
+  function initEvents() {
+    handleEl.addEventListener("mousedown", () => {
+      drugging = true;
+    });
+    window.addEventListener("mouseup", () => {
+      drugging = false;
+    });
+
+    window.addEventListener("mousemove", (event) => {
+      if (drugging) {
+        move(event.clientX);
+      }
+    });
+  }
+
+  function adjustImageSize() {
+    imagesContainerElWidth = imagesContainerEl.offsetWidth;
     img1El.style.width = imagesContainerElWidth + "px";
     img2El.style.width = imagesContainerElWidth + "px";
-  });
+  }
+  window.addEventListener("resize", adjustImageSize);
+  adjustImageSize();
+  initEvents();
 });
