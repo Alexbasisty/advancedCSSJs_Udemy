a6278c69099227ea8b0c66fb56ec007578c91a23
Alex Basystyi <alexbasisty@gmail.com>
2020-05-21 12:55:08 +0200

    images width on resize

--- projects/sliding_gallery/js/app.js
+++ projects/sliding_gallery/js/app.js
@@ -1,1 +1,13 @@
 import "../sass/style.scss";
+
+document.addEventListener("DOMContentLoaded", () => {
+  const imagesContainerEl = document.querySelector(".slider__images-container");
+  const img1El = document.querySelector(".slider__image-container--first img");
+  const img2El = document.querySelector(".slider__image-container--second img");
+
+  window.addEventListener("resize", () => {
+    const imagesContainerElWidth = imagesContainerEl.offsetWidth;
+    img1El.style.width = imagesContainerElWidth + "px";
+    img2El.style.width = imagesContainerElWidth + "px";
+  });
+});
