49505f325fc81dd1f38d7b0722392e4f4d847b69
Alex Basystyi <alexbasisty@gmail.com>
2020-06-04 12:32:25 +0200

    rendering list for mobile desktop, &styling

--- projects/RWD_optimization/js/main.js
+++ projects/RWD_optimization/js/main.js
@@ -29,12 +29,42 @@ const valuesContainer = document.querySelector(".app--values");
 
 const desktopViewport = window.matchMedia("screen and (min-width: 500px)");
 
+const drawMobileValues = () => {
+    valuesContainer.innerHTML = "";
+    const list = document.createElement("ul");
+
+    values.forEach((value) => {
+        let element = document.createElement("li");
+
+        let name = document.createElement("div");
+        name.innerHTML = `<strong>Nazwa:</strong> ${value.name}`;
+
+        let calories = document.createElement("div");
+        calories.innerHTML = `<strong>Kalorie:</strong> ${value.calories}`;
+
+        let fat = document.createElement("div");
+        fat.innerHTML = `<strong>Tłuszcz:</strong> ${value.fat}`;
+
+        let carbs = document.createElement("div");
+        carbs.innerHTML = `<strong>Węgłowodany:</strong> ${value.carbs}`;
+
+        element.appendChild(name);
+        element.appendChild(calories);
+        element.appendChild(fat);
+        element.appendChild(carbs);
+
+        list.appendChild(element);
+    });
+
+    valuesContainer.appendChild(list);
+};
+
 const drawDesktopValues = () => {
     valuesContainer.innerHTML = "";
     let table = document.createElement("table");
     let thead = document.createElement("thead");
     thead.innerHTML =
-        "<tr><th>Nazwa</th><th>Kalories</th><th>Tłuszcz</th><th>Węgłowodany</th></tr>";
+        "<tr><th>Nazwa</th><th>Kalorie</th><th>Tłuszcz</th><th>Węgłowodany</th></tr>";
     let tbody = document.createElement("tbody");
 
     values.forEach((value) => {
@@ -49,4 +79,5 @@ const drawDesktopValues = () => {
     valuesContainer.appendChild(table);
 };
 
-drawDesktopValues();
+// drawDesktopValues();
+drawMobileValues();
