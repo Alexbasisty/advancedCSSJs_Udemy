db1ca7414c5e9c6c4ee97a9307b0ac3a3cf4709f
Alex Basystyi <alexbasisty@gmail.com>
2020-06-11 15:08:59 +0200

    night sky with animation

--- projects/CANVAS/js/main.js
+++ projects/CANVAS/js/main.js
@@ -10,10 +10,68 @@ class Sky {
         this.canvas.width = this.width;
         this.canvas.height = this.height;
 
-        this.ctx.fillStyle = "#000000";
+        this.ctx.fillStyle = "#000";
         this.ctx.fillRect(0, 0, this.width, this.height);
     }
-    
+
+    generateStars(count) {
+        let stars = [];
+
+        for (let i = 0; i < count; i++) {
+            const radius = Math.random() * 3 + 2;
+
+            stars.push({
+                x: Math.random() * this.width,
+                y: Math.random() * this.height,
+                radius: radius,
+                originalRadius: radius,
+                color: "#fff",
+                speed: Math.random() + 0.25,
+            });
+        }
+        this.stars = stars;
+    }
+
+    drawStars() {
+        this.stars.forEach((star) => {
+            this.drawStar(star);
+        });
+    }
+
+    updateStars() {
+        this.stars.forEach((star) => {
+            star.x += star.speed;
+            star.y -= (star.speed * (this.width / 2 - star.x)) / 3000;
+            star.radius = (star.originalRadius * Math.random()) / 4 + 0.9;
+
+            if (star.x > this.width + 2 * star.radius) {
+                star.x = -2 * star.radius;
+            }
+        });
+    }
+
+    drawOverLayer() {
+        let gradient = this.ctx.createRadialGradient(
+            this.width / 2,
+            this.height / 2,
+            250,
+            this.width / 2,
+            this.height / 2,
+            this.width / 2
+        );
+
+        gradient.addColorStop(0, "rgba(0,0,0,0)");
+        gradient.addColorStop(1, "rgba(0,0,0,0.75)");
+
+        this.ctx.fillStyle = gradient;
+        this.ctx.fillRect(0, 0, this.width, this.height);
+    }
+
+    clearCanvas() {
+        this.ctx.fillStyle = "#000";
+        this.ctx.fillRect(0, 0, this.width, this.height);
+    }
+
     drawStar(star) {
         this.ctx.save();
 
@@ -36,13 +94,18 @@ class Sky {
     }
 
     draw() {
-        console.log("draw");
+        this.clearCanvas();
+        this.drawStars();
+        this.updateStars();
+        this.drawOverLayer();
         window.requestAnimationFrame(() => this.draw());
     }
 
     run() {
         this.initCanvas();
+        this.generateStars(500);
         this.draw();
+    }
 }
 
 const sky = new Sky(document.querySelector("#canvas"));
