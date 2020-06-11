700813b5b397ea334cc722dfe49272690e471739
Alex Basystyi <alexbasisty@gmail.com>
2020-06-11 14:04:37 +0200

    drawing canvas on the whole screen

--- projects/CANVAS/js/main.js
+++ projects/CANVAS/js/main.js
@@ -1,14 +1,23 @@
-const canvas = document.querySelector("#canvas");
-const ctx = canvas.getContext("2d");
+class Sky {
+    constructor(canvas) {
+        this.canvas = canvas;
+        this.ctx = canvas.getContext("2d");
+        this.width = window.innerWidth;
+        this.height = window.innerHeight;
+    }
 
-ctx.fillStyle = "#ff0000";
-ctx.fillRect(0, 0, 100, 50);
+    initCanvas() {
+        this.canvas.width = this.width;
+        this.canvas.height = this.height;
 
-ctx.beginPath();
-ctx.lineWidth = 3;
-ctx.strokeStyle = "#00ff00";
-ctx.moveTo(50, 50);
-ctx.lineTo(100, 100);
-ctx.lineTo(200, 150);
-// ctx.closePath();
-ctx.stroke();
+        this.ctx.fillStyle = "#000000";
+        this.ctx.fillRect(0, 0, this.width, this.height);
+    }
+
+    run() {
+        this.initCanvas();
+    }
+}
+
+const sky = new Sky(document.querySelector("#canvas"));
+sky.run();
