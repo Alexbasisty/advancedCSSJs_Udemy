950fbd7a92e5bda35a741c2ef3181ba1776ff2ea
Alex Basystyi <alexbasisty@gmail.com>
2020-06-13 14:01:54 +0200

    requestAnimationFrame method testing

--- Study/spinner_animation/spinner.js
+++ Study/spinner_animation/spinner.js
@@ -10,3 +10,36 @@ const rotate = () => {
 };
 
 setInterval(rotate, fps);
+
+/*
+ requestAnimationFrame
+*/
+
+const rAFDiv = document.querySelector(".rAF");
+let time = 0,
+    frames = 0,
+    beforeTime = performance.now(),
+    result = 0;
+
+const counter = (currentTime) => {
+    if (currentTime - beforeTime > 100) {
+        result++;
+        beforeTime = currentTime;
+    }
+
+    frames++;
+    rAFDiv.textContent = `Czas: ${time.toFixed(
+        1
+    )}; Liczba frame-ów: ${frames}; fps: ${(
+        frames / time
+    ).toFixed()}; Punkty: ${result}; Aktualny czas wywołania rAF: ${currentTime}`;
+    requestAnimationFrame(counter);
+};
+
+const timer = () => {
+    time += 0.1;
+    setTimeout(timer, 100);
+};
+
+timer();
+counter();
