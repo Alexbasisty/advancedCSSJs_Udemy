1711dbe9d9285396092acfef62d7d583ce0096d1
Alex Basystyi <alexbasisty@gmail.com>
2020-06-15 12:46:03 +0200

    the whole working animation

--- projects/animation_spring/animation.js
+++ projects/animation_spring/animation.js
@@ -8,6 +8,9 @@ const stretchSpring = () => {
     spring.style.animationPlayState = "running";
 
     btn.textContent = "Puść sprężynę";
+
+    btn.removeEventListener("mousedown", stretchSpring);
+    btn.removeEventListener("touchstart", stretchSpring);
 };
 
 const releaseSpring = () => {
@@ -20,7 +23,27 @@ const releaseSpring = () => {
     const powerPercent = (fillWidth / barWidth).toFixed(2);
 
     btn.style.color = "black";
-    btn.textContent = `Moc uderżenia to ${powerPercent * 100}%`;
+    btn.textContent = `Moc uderżenia to ${(powerPercent * 100).toFixed()}%`;
+    fill.style.animationPlayState = "paused";
+
+    document.documentElement.style.setProperty(
+        "--power-x",
+        `${30 + powerPercent * 60}%`
+    );
+    ball.style.animation =
+        "fly-ball-x 1s 1 forwards .15s cubic-bezier(0.17, 0.67, 0.6, 1), fly-ball-y 1s 1 forwards .15s linear";
+
+    document.documentElement.style.setProperty(
+        "--spring-left",
+        getComputedStyle(spring).left
+    );
+
+    spring.style.animation = "release-spring 0.2s forwards linear";
+
+    //zabłokowanie kliknięcia
+
+    btn.removeEventListener("mouseup", stretchSpring);
+    btn.removeEventListener("touchend", stretchSpring);
 };
 
 const resetAnimation = () => {
